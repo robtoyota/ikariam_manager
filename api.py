ce78c105ba3f6c2ba12b70b740009a666e448af7
RT <49793997+robtoyota@users.noreply.github.com>
2020-04-04 21:16:19 -0400

    Setup config file and DB connection

--- api.py
+++ api.py
@@ -1,10 +1,21 @@
+from pg import Pg
 from city import City
 from resource import Resource
 
+
 class API:
-	def __init__(self):
+	def __init__(self, config_dict):
+		# Load the config that was provided
+		self.config = config_dict
+
+	def __enter__(self):
 		# Connect to the DB
-		pass
+		self.db = Pg(self.config)
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		# Close the DB when done with this instance
+		self.db.close()
 
 	def upsert_city(self, city: City):
 		pass
